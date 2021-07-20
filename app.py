5505d1e17486e2ae688f0bd3b69317b10a355f90
Andra Malmen <andramalmen@gmail.com>
2021-07-20 23:01:05 +0300

    Add find similar words

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,8 +7,20 @@ data = json.load(open("data.json"))
 def translate(word):
     try:
         return data[word.lower()]
-    except:
-        return "The word does not exist. Please double check it!"
+    except Exception as err:
+        closeMatches = get_close_matches(word, data.keys(), n=1)
+        if len(closeMatches) == 0:
+            return "The word does not exist. Please double check it!"
+        else:
+            response = input(
+                'Did you mean "%s"? Enter Y for yes and N for no ' %
+                closeMatches[0])
+            if response == "Y":
+                return data[closeMatches[0]]
+            elif response == "N":
+                return "The word does not exist. Please double check it!"
+            else:
+                return "Option not available"
 
 
 word = input("Enter the word:")
